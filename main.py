e2f2c00f856b28127d8fc992138b82fe989e0f60
corbin <c_luck@mail.fhsu.edu>
2026-02-22 18:34:04 -0600

    Testing plotting data

--- main.py
+++ main.py
@@ -2,19 +2,31 @@
 ### Corbin Luck
 ### Mini Project 2
 
-## Question: How many male patients have A+ blood type?
+## Question: How many patients have obesity?
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.Dataframe(name)
+
+df = pd.read_csv("./data/healthcare_dataset.csv", header=None)
+
+condition = df[4]
+
+obesity_count = (condition == "Obesity").value_counts()
+#obese = obesity_count[True]
+#not_obese = obesity_count[False]
+
+obesity_count.plot(kind="bar")
+plt.title("Obesity Distribution")
+plt.ylabel("Number of Patients")
+plt.xticks(rotation=45)
+plt.xticks(fontsize=7)
+plt.xticks([0,1], ["Not Obese","Obese"])
+plt.show()
 
 
 # (5/5 points) Proper import of packages used.
 # (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
-# Think of some question you would like to solve such as:
-# "How many homes in the US have access to 100Mbps Internet or more?"
-# "How many movies that Ridley Scott directed is on Netflix?" - https://www.kaggle.com/datasets/shivamb/netflix-shows
-# Here are some other great datasets: https://www.kaggle.com/datasets
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
