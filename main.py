5690d1c7435cb17b77a5294d9c233f183458d5b0
corbin <c_luck@mail.fhsu.edu>
2026-02-22 18:41:43 -0600

    Changes to the graph

--- main.py
+++ main.py
@@ -2,34 +2,34 @@
 ### Corbin Luck
 ### Mini Project 2
 
-## Question: How many patients have obesity?
-
+## Imports
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
+## sets df as the data in the healthcare_dataset.csv
 df = pd.read_csv("./data/healthcare_dataset.csv", header=None)
 
+## Selects just the condition column from the datasheet
 condition = df[4]
 
+## Finds the count of patients with and without Obesity
 obesity_count = (condition == "Obesity").value_counts()
-#obese = obesity_count[True]
-#not_obese = obesity_count[False]
 
-obesity_count.plot(kind="bar")
+## Plots a bar graph to compare the obese vs not obese patients
+obesity_count.plot(
+    kind="bar",
+    color=["green", "red"]
+)
 plt.title("Obesity Distribution")
 plt.ylabel("Number of Patients")
-plt.xticks(rotation=45)
-plt.xticks(fontsize=7)
-plt.xticks([0,1], ["Not Obese","Obese"])
+plt.xticks([0,1], ["Not Obese", "Obese"], rotation=45, fontsize="7")
 plt.show()
 
 
-# (5/5 points) Proper import of packages used.
-# (20/20 points) Using a data source of your choice, such as data from data.gov or using the Faker package, generate or retrieve some data for creating basic statistics on. This will generally come in as json data, etc.
+
+
 # (10/10 points) Store this information in Pandas dataframe. These should be 2D data as a dataframe, meaning the data is labeled tabular data.
 # (10/10 points) Using matplotlib, graph this data in a way that will visually represent the data. Really try to build some fancy charts here as it will greatly help you in future homework assignments and in the final project.
 # (10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder, the project should save these when it executes. You may want to add this folder to your .gitignore file.
-# (10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
 # (10/10 points) I will be checking out the main branch of your project. Please be sure to include a requirements.txt file which contains all the packages that need installed. You can create this fille with the output of pip freeze at the terminal prompt.
 # (20/20 points) There should be a README.md file in your project that explains what your project is, how to install the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown. Be thorough on the explanations.
